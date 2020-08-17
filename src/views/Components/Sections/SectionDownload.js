4c72e8a6e2c58b8ddc9ebeb9267d878f9d094d4a
Hải Sơn Nguyễn <nguyensonhai1009@gmail.com>
2020-08-18 03:21:43 +0700

    update

--- src/views/Components/Sections/SectionDownload.js
+++ src/views/Components/Sections/SectionDownload.js
@@ -19,15 +19,10 @@ export default function SectionDownload() {
         <GridContainer className={classes.textCenter} justify="center">
           <GridItem xs={12} sm={12} md={8}>
             <h2>Get It Now</h2>
-            <h4>
-              Healthy is something that is on everyone’s mind at some point. We
-              put a bit of a focus on eating, fitness and plan your healthy life
-              cycle with this one. We have more colorful style features for you.
-            </h4>
           </GridItem>
           <GridItem xs={12} sm={8} md={6}>
             <Button
-              color="rose"
+              color="github"
               size="lg"
               href="https://firebasestorage.googleapis.com/v0/b/ez-health.appspot.com/o/android%2Fezhealth-v.1.0.0.apk?alt=media&token=450f0d1e-7ba2-4ae3-ac5e-b944b515cf9b"
               target="_blank"
